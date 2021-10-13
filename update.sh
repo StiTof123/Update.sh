3dc6273b0b369363d1b84be47c3b3ad6fcda4418
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-14 00:49:04 +0200

    added autoremove

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ssh 192.168.178.23 sudo apt update && sudo apt full-upgrade -y
-ssh 192.168.178.38 sudo apt update && sudo apt full-upgrade -y
-ssh 192.168.178.40 sudo apt update && sudo apt full-upgrade -y
-ssh 192.168.178.50 sudo apt update && sudo apt full-upgrade -y
-ssh 192.168.178.28 sudo pacman -Syu --noconfirm && sudo yes | flatpak update  
+ssh 192.168.178.23 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+ssh 192.168.178.38 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+ssh 192.168.178.40 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+ssh 192.168.178.50 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+ssh 192.168.178.28 sudo pacman -Syu --noconfirm && yes | sudo flatpak update && yes | sudo snap refresh   
+ssh 192.168.178.30 sudo pacman -Syu --noconfirm && yes | sudo flatpak update 
